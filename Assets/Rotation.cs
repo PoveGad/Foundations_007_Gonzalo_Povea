51f3c0babb6965c94842375f4d2a2e77a4da2231
GonzaPG27 <a20193775@pucp.edu.pe>
2024-04-10 10:42:33 -0500

    One last dance

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -1,27 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using Random = UnityEngine.Random;
 
 public class Rotation : MonoBehaviour
 {
     [SerializeField] private Transform Dial;
     [SerializeField] private int SnapAmount = 15;
     [SerializeField] private float AngleTolerance;
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private TMP_Text _text2;
+    private int number;
+    [SerializeField] private GameObject _dial;
+    
+    
 
     private float _valuefixed;
+    private float startAngleInteractor;
+    private int i =0;
 
     private XRGrabInteractable grabInteractor => GetComponent<XRGrabInteractable>();
     private XRBaseInteractor interactor;
     private bool grabbing;
     private float startangle;
+    
 
     private void Start()
     {
+        GenerateNumber();
         grabInteractor.selectEntered.AddListener(Grabbed);
         grabInteractor.selectExited.AddListener(GrabbedExited);
+        _text.text = "0";
         
+
+    }
+
+    private void GenerateNumber()
+    {
+        number = Random.Range(1, 360);
+        _text2.text = "Password: " + number.ToString();
     }
 
     private void GrabbedExited(SelectExitEventArgs arg0)
@@ -33,7 +53,8 @@ public class Rotation : MonoBehaviour
     {
         interactor = GetComponent<XRGrabInteractable>().selectingInteractor;
         grabbing = true;
-        startangle = 0f;
+        startangle = Dial.rotation.eulerAngles.z;
+        startAngleInteractor = GetInteractorRoation();
     }
 
     private void Update()
@@ -44,18 +65,35 @@ public class Rotation : MonoBehaviour
             GetRotationDistance(rotationAngle);
         }
     }
-
-    
-
+  
     public float GetInteractorRoation() => interactor.GetComponent<Transform>().eulerAngles.z;
 
     private void GetRotationDistance(float currentAngle)
     {
-        var angleDifference = Mathf.Abs(0f - currentAngle);
-        _valuefixed = angleDifference;
-        Debug.Log(angleDifference);
+        var angleDifference = Mathf.Abs(startAngleInteractor - currentAngle);
+        var newDialRotation = startangle + angleDifference;
+        Dial.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y,
+            newDialRotation);
         
+        _text.text = ((int)Dial.rotation.eulerAngles.z).ToString();
+       
+        if ((int)Dial.rotation.eulerAngles.z == number)
+        {
+            i++;
+            if (i == 3)
+            {
+                UnlockDoor();
+            }
+            else
+            {
+                GenerateNumber();
+            }
+        }
     }
-    
-    
+
+    private void UnlockDoor()
+    {
+        _dial.SetActive(false);
+        _text2.text = "Unlocked";
+    }
 }
